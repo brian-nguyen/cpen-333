4f5e347fbaae2cc1e0702f5119ea9f33e5dc8778
Brian Nguyen <briannguyen992@gmail.com>
2017-09-16 22:08:45 -0700

    Implemented quicksort. Add test function

--- lab2/src/quicksort.cpp
+++ lab2/src/quicksort.cpp
@@ -2,18 +2,45 @@
 #include <thread>
 #include <vector>
 
+// checks if sorted
+bool isSorted(std::vector<int>& data) {
+  for (int i = 0; i < data.size() - 1; i++) {
+    if (data[i] > data[i + 1]) return false;
+  }
+  return true;
+}
+
+// utility to print all elements
+void printArray(std::vector<int>& data) {
+  for (int k = 0; k < data.size(); k++) {
+    std::cout << data[k] << " ";
+  }
+  std::cout << std::endl;
+}
+
 // partitions elements low through high (inclusive)
 // around a pivot and returns the pivot index
 // chooses the highest element as pivot
 size_t partition(std::vector<int>& data, int low, int high) {
   int pivot = data[high];
-  int i = low;
-  int j = high;
-  
-  while (i < j) {
+  int i = low - 1;
 
+  // go through array and swap smaller
+  // element with one greater than the pivot
+  for (int j = low; j < high - 1; j++) {
+    if (data[j] <= pivot) {
+      // move left pointer forward
+      i++;
+      std::swap(data[i], data[j]);
     }
   }
+
+  // move pivot into position ahead 
+  // of where left pointer stopped
+  // this is the partitioning index
+  std::swap(data[i + 1], pivot);
+  printArray(data);
+  return i + 1;
 }
 
 void quicksort(std::vector<int>& data, int low, int high) {
@@ -32,19 +59,31 @@ void parallel_quicksort(std::vector<int>& data, int low, int high) {
 int main() {
 
   // create two copies of random data
-  const int VECTOR_SIZE = 1000000;
+  const int VECTOR_SIZE = 10;
   std::vector<int> v1(VECTOR_SIZE, 0);
   // fill with random integers
-  for (int i=0; i<VECTOR_SIZE; ++i) {
+  for (int i=0; i < VECTOR_SIZE; ++i) {
     v1[i] = rand();
   }
   std::vector<int> v2 = v1;  // copy all contents
 
   // sort v1 using sequential algorithm
+  printArray(v1);
   quicksort(v1, 0, v1.size() - 1);
-
   // sort v2 using parallel algorithm
   parallel_quicksort(v2, 0, v2.size() - 1);
+
+  if (isSorted(v1)) {
+    std::cout << "SEQUENTIAL SORT PASSED" << std::endl;
+  } else {
+    std::cout << "SEQUENTIAL SORT FAILED" << std::endl;
+  }
+
+  if (isSorted(v2)) {
+    std::cout << "PARALLEL SORT PASSED" << std::endl;
+  } else {
+    std::cout << "PARALLEL SORT FAILED"
+  }
 
   return 0;
 }
